10776d8278f7c8884c631f4a458bfa850342ea2e
Polister <yyla05@foxmail.com>
2024-10-15 00:35:07 +0800

    fix: i18n补齐

--- translation/capture_zh.ts
+++ translation/capture_zh.ts
@@ -34,68 +34,86 @@
         <translation>开机自启动</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.ui" line="112"/>
+        <location filename="CaptureApplication.ui" line="139"/>
         <source>修改截图快捷键</source>
         <translation>修改截图快捷键</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.ui" line="118"/>
+        <location filename="CaptureApplication.ui" line="105"/>
         <source>配置</source>
         <translation>设置</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.ui" line="133"/>
+        <location filename="CaptureApplication.ui" line="120"/>
         <source>语言：</source>
         <translation>语言</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="31"/>
-        <location filename="CaptureApplication.cpp" line="227"/>
+        <location filename="CaptureApplication.cpp" line="34"/>
+        <location filename="CaptureApplication.cpp" line="259"/>
         <source>Dian=
 screenshots:F1</source>
         <translation>点截
 截图：</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="34"/>
         <source>Dian-capture
 截屏:F1
 贴图:鼠标中键</source>
-        <translation>Dian-capture
+        <translation type="vanished">Dian-capture
 截屏:F1
 贴图:鼠标中键</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="39"/>
-        <location filename="CaptureApplication.cpp" line="64"/>
+        <location filename="CaptureApplication.cpp" line="40"/>
+        <location filename="CaptureApplication.cpp" line="80"/>
         <source>Preference</source>
         <translation>偏好设置</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="41"/>
-        <location filename="CaptureApplication.cpp" line="65"/>
+        <location filename="CaptureApplication.cpp" line="42"/>
+        <location filename="CaptureApplication.cpp" line="81"/>
         <source>Quit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="51"/>
+        <location filename="CaptureApplication.cpp" line="53"/>
         <source>Error</source>
         <translation>错误</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="51"/>
-        <source>register shorcut F1 failed</source>
-        <translation>注册快捷键F1失败（可能被占用）</translation>
+        <location filename="CaptureApplication.cpp" line="53"/>
+        <source>register shorcut failed:</source>
+        <translation>注册截图快捷键失败： </translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="66"/>
+        <location filename="CaptureApplication.cpp" line="71"/>
+        <source>Dian-capture</source>
+        <translation>Dian-capture</translation>
+    </message>
+    <message>
+        <location filename="CaptureApplication.cpp" line="72"/>
+        <source>截屏:</source>
+        <translation>截屏： </translation>
+    </message>
+    <message>
+        <location filename="CaptureApplication.cpp" line="75"/>
+        <source>贴图:鼠标中键</source>
+        <translation>贴图：鼠标中键</translation>
+    </message>
+    <message>
+        <source>register shorcut F1 failed</source>
+        <translation type="vanished">注册快捷键F1失败（可能被占用）</translation>
+    </message>
+    <message>
+        <location filename="CaptureApplication.cpp" line="82"/>
         <source>Dian-capture
 screenshots:F1</source>
         <translation>点截
 截图：</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="86"/>
+        <location filename="CaptureApplication.cpp" line="102"/>
         <source>Choose Image</source>
         <translation>选择图片</translation>
     </message>
@@ -111,32 +129,32 @@ screenshots:F1</source>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="CaptureApplication.cpp" line="217"/>
+        <location filename="CaptureApplication.cpp" line="247"/>
         <source>修改截图快捷键</source>
         <translation>修改截图快捷键</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="218"/>
+        <location filename="CaptureApplication.cpp" line="248"/>
         <source>输入快捷键（例如：Alt+F1 或 F1）:</source>
         <translation>输入快捷键（例如：Alt+F1 或 F1）:</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="224"/>
+        <location filename="CaptureApplication.cpp" line="254"/>
         <source>错误</source>
         <translation>修改失败</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="224"/>
+        <location filename="CaptureApplication.cpp" line="254"/>
         <source>快捷键已被占用：</source>
         <translation>快捷键已被占用：</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="226"/>
+        <location filename="CaptureApplication.cpp" line="256"/>
         <source>成功</source>
         <translation>修改成功</translation>
     </message>
     <message>
-        <location filename="CaptureApplication.cpp" line="226"/>
+        <location filename="CaptureApplication.cpp" line="256"/>
         <source>快捷键已修改：</source>
         <translation>快捷键已修改：</translation>
     </message>
@@ -177,57 +195,57 @@ screenshots:F1</source>
 <context>
     <name>ScreenView</name>
     <message>
-        <location filename="screenview.cpp" line="211"/>
+        <location filename="screenview.cpp" line="212"/>
         <source>保存图片</source>
         <translation>保存图片</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="359"/>
+        <location filename="screenview.cpp" line="360"/>
         <source>选择颜色</source>
         <translation>选择颜色</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="514"/>
+        <location filename="screenview.cpp" line="517"/>
         <source>填充</source>
         <translation>填充</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="640"/>
+        <location filename="screenview.cpp" line="643"/>
         <source>复制到剪贴板(Enter)</source>
         <translation>复制到剪贴板(Enter)</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="641"/>
+        <location filename="screenview.cpp" line="644"/>
         <source>保存到文件(S)</source>
         <translation>另存为...(S)</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="642"/>
+        <location filename="screenview.cpp" line="645"/>
         <source>绘制线段(L)</source>
         <translation>绘制线段(L)</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="643"/>
+        <location filename="screenview.cpp" line="646"/>
         <source>绘制矩形(P)</source>
         <translation>绘制矩形(P)</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="644"/>
+        <location filename="screenview.cpp" line="647"/>
         <source>绘制椭圆(M)</source>
         <translation>绘制椭圆(M)</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="645"/>
+        <location filename="screenview.cpp" line="648"/>
         <source>添加文本(T)</source>
         <translation>添加文本(T)</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="646"/>
+        <location filename="screenview.cpp" line="649"/>
         <source>上传到PicGo(U)</source>
         <translation>上传到PicGo(U)</translation>
     </message>
     <message>
-        <location filename="screenview.cpp" line="647"/>
+        <location filename="screenview.cpp" line="650"/>
         <source>提取文字(O)</source>
         <translation>提取文字(O)</translation>
     </message>
